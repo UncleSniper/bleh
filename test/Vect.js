9004a68a00760ab51fa526d7072525729d23b4c2
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2018-08-02 15:03:21 +0000

    more

--- test/Vect.js
+++ test/Vect.js
@@ -24,10 +24,22 @@ function tm_constructor() {
 
 function tp_row() {
 	it('should return the y property', () => expect(new Vect(42, 1337).row).to.equal(1337));
+	it('should set the y property', () => {
+		const vec = new Vect(1, 2);
+		vec.row = 5;
+		expect(vec.x).to.equal(1);
+		expect(vec.y).to.equal(5);
+	});
 }
 
 function tp_column() {
 	it('should return the x property', () => expect(new Vect(42, 1337).column).to.equal(42));
+	it('should set the x property', () => {
+		const vec = new Vect(1, 2);
+		vec.column = 5;
+		expect(vec.x).to.equal(5);
+		expect(vec.y).to.equal(2);
+	});
 }
 
 t_Vect.test();
