023b044c5cf270bcc685bddd475ff7310a20c24c
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2018-08-06 15:19:14 +0000

    more

--- mkdoc.sh
+++ mkdoc.sh
@@ -2,5 +2,6 @@
 
 cd "$(dirname -- "${BASH_SOURCE-$0}")"
 
-rm -rf apidoc
-jsdoc -P package.json -r -d apidoc -t /usr/lib/node_modules/jsdoc-baseline bleh.js lib
+rm -rf apidoc &&
+mkdir -p apidoc &&
+asciidoc -b html -o apidoc/index.html doc/api/index.adoc
